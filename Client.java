7bb21a6825a270bc5c6cff182e3ac9a523e3e9ac
gone-phishing <riteshoneinamillion@gmail.com>
2015-10-20 02:51:40 +0530

    Removing the greet parts from client and server

--- Client.java
+++ Client.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class GreetingClient
+public class Client
 {
    public static void main(String [] args)
    {
@@ -9,21 +9,18 @@ public class GreetingClient
       int port = Integer.parseInt(args[1]);
       try
       {
-         System.out.println("Connecting to " + serverName +
-		 " on port " + port);
+         System.out.println("Connecting to " + serverName +" on port " + port);
          Socket client = new Socket(serverName, port);
-         System.out.println("Just connected to " 
-		 + client.getRemoteSocketAddress());
+         System.out.println("Just connected to " + client.getRemoteSocketAddress());
          OutputStream outToServer = client.getOutputStream();
          DataOutputStream out = new DataOutputStream(outToServer);
-         out.writeUTF("Hello from "
-                      + client.getLocalSocketAddress());
+         out.writeUTF("Hello from "+ client.getLocalSocketAddress());
          InputStream inFromServer = client.getInputStream();
-         DataInputStream in =
-                        new DataInputStream(inFromServer);
+         DataInputStream in = new DataInputStream(inFromServer);
          System.out.println("Server says " + in.readUTF());
          client.close();
-      }catch(IOException e)
+      }
+      catch(IOException e)
       {
          e.printStackTrace();
       }
