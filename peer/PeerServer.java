a2bc82712ef49fb8be018095954ebdd45abff6f1
gone-phishing <riteshoneinamillion@gmail.com>
2015-11-03 04:27:29 +0530

    Max download limit for a client set

--- peer/PeerServer.java
+++ peer/PeerServer.java
@@ -7,6 +7,8 @@ class PeerServer extends Thread
 	private int port;
 	public static int count = 0;
 	public static boolean listen = true;
+	private int MAX_CONN = 5;
+
 	public PeerServer(int port)
 	{
 		this.port = port;
@@ -14,7 +16,6 @@ class PeerServer extends Thread
 
 	public void run()
 	{
-		//boolean listen = true;
 		try
 		(
 			ServerSocket ss = new ServerSocket(port);
@@ -25,6 +26,20 @@ class PeerServer extends Thread
 			{
 				new PeerServerHandler(ss.accept()).start();
 				count++;
+				if(count == MAX_CONN)
+				{
+					while(count == MAX_CONN)
+					{
+						try
+						{
+							Thread.sleep(10000);
+						}
+						catch(InterruptedException ie)
+						{
+							ie.printStackTrace();
+						}
+					}
+				}
 			}
 		}
 		catch(IOException e1)
